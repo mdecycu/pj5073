ce6daa159a2372ae8b5915644ef254085251f4da
mdecourse <mdecourse@gmail.com>
2021-01-25 21:22:44 +0800

    reorangized project site

--- init.py
+++ init.py
@@ -10,7 +10,9 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "JCAD Project Site"
+    site_title = "JCAD Project"
+    ip = "127.0.0.1"
+    port = 9443
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
