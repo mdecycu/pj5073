50e3f2db7853d32dfe37e79d602a562fba201b56
mdecourse <mdecourse@gmail.com>
2021-12-28 21:48:15 +0800

    add Nginx autoindex .htpasswd page

--- init.py
+++ init.py
@@ -11,8 +11,7 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "pj5073"
-    #ip = "127.0.0.1"
-    ip = "59.127.237.33"
+    ip = "127.0.0.1"
     dynamic_port = 9443
     static_port = 8443
     def __init__(self):
